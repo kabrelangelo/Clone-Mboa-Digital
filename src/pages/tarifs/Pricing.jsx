d858c08b8d8f6bc89f3b66344ad983b1687e3b01
unknown <ngoumkwa.kenne@gmail.com>
2024-09-02 15:55:56 +0100

    Page Tarification

--- src/pages/tarifs/Pricing.jsx
+++ src/pages/tarifs/Pricing.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Evaluation from './Evaluation'
 
 export default function Pricing() {
   return (
-    <div>Pricing</div>
+    <div>
+      <Evaluation/>
+    </div>
   )
 }
